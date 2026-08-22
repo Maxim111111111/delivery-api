c3a1ca6601f7667a1892538ed8915029be109eed
Maxim Vasilev <msprog@icloud.com>
2026-08-22 10:44:54 +0400

    test: add tests for order validation

---
+++ internal/service/order_test.go
@@ -0,0 +1,84 @@
+package service
+
+import (
+	"context"
+	"delivery-api/internal/model"
+	"testing"
+)
+
+var _ OrderRepository = (*fakeRepo)(nil)
+
+type fakeRepo struct {
+	order model.Order
+	err   error
+}
+
+func (f *fakeRepo) GetOrders(ctx context.Context) ([]model.Order, error) {
+	return nil, f.err
+}
+
+func (f *fakeRepo) GetOrderByID(ctx context.Context, id int) (model.Order, error) {
+	return f.order, f.err
+}
+
+func (f *fakeRepo) CreateOrder(ctx context.Context, o model.Order) (model.Order, error) {
+	return f.order, f.err
+}
+
+func (f *fakeRepo) UpdateOrder(ctx context.Context, id int, o model.Order) (model.Order, error) {
+	return f.order, f.err
+}
+
+func (f *fakeRepo) DeleteOrder(ctx context.Context, id int) error {
+	return f.err
+}
+
+func TestValidateOrder(t *testing.T) {
+	tests := []struct {
+		name    string
+		order   model.Order
+		wantErr bool
+	}{
+		{name: "correctOrder",
+			order: model.Order{Address: "г. Ижевск, ул. Героя России Ильфата-Закирова, д.2, кв.3",
+				Price: 137000,
+				Items: []model.OrderItem{{
+					Name:     "Пицца Маргарита",
+					Quantity: 2,
+					Price:    50000,
+				}}},
+			wantErr: false},
+		{name: "emptyAddress",
+			order: model.Order{Address: "",
+				Price: 137000,
+				Items: []model.OrderItem{{
+					Name:     "Пицца Маргарита",
+					Quantity: 2,
+					Price:    50000,
+				}}},
+			wantErr: true},
+		{name: "emptyItems",
+			order: model.Order{Address: "г. Ижевск, ул. Героя России Ильфата-Закирова, д.2, кв.3",
+				Price: 137000,
+				Items: []model.OrderItem{}},
+			wantErr: true},
+		{name: "zeroItemsQuantity",
+			order: model.Order{Address: "г. Ижевск, ул. Героя России Ильфата-Закирова, д.2, кв.3",
+				Price: 137000,
+				Items: []model.OrderItem{{
+					Name:     "Пицца Маргарита",
+					Quantity: 0,
+					Price:    50000,
+				}}},
+			wantErr: true},
+	}
+
+	for _, test := range tests {
+		t.Run(test.name, func(t *testing.T) {
+			err := validateOrder(test.order)
+			if (err != nil) != test.wantErr {
+				t.Errorf("validateOrder() error: %v, wantErr: %v", err, test.wantErr)
+			}
+		})
+	}
+}
